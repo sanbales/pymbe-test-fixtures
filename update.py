40508e9aa7dfdf2485c4529dec550a47320d16a2
Santiago Balestrini <santiago.balestrini@gtri.gatech.edu>
2021-09-11 18:47:06 -0400

    Updated the download_all_projects functions

--- update.py
+++ update.py
@@ -3,13 +3,14 @@ from pathlib import Path
 from warnings import warn
 
 import pymbe.api as pm
+from pymbe.client import APIClient
 
 
 PYMBE_ROOT = Path(pm.__file__).parent
 FIXTURES = (PYMBE_ROOT / "../../tests/fixtures").resolve()
 
 
-def get_latest_by_name(client: pm.SysML2ClientWidget):
+def get_latest_by_name(client: APIClient):
     projects = defaultdict(list)
 
     for id_, data in client.projects.items():
@@ -27,19 +28,16 @@ def get_latest_by_name(client: pm.SysML2ClientWidget):
 
 
 def download_all_projects(url: str, port: int = 9000):
-    client = pm.SysML2ClientWidget(host_url=url, host_port=port)
+    client = APIClient(host_url=url, host_port=port)
 
     for project_name, project_id in get_latest_by_name(client).items():
-        print(f"Retrieving '{project_name}'")
-        client.selected_project = project_id
         try:
-            model = pm.Model.load(
-                elements=client._retrieve_data(client.elements_url),
-                name=project_name,
-                source=client.elements_url,
-            )
+            print(f"Retrieving '{project_name}'")
+            client.selected_project = project_id
+            model = client.get_model()
+
             print(f" > Saving {project_name}")
-            model.save_to_file(FIXTURES / f"{project_name}.json")
+            model.save_to_file(FIXTURES / project_name)
             print(f" > Successfully Downloaded '{project_name}'")
         except Exception as exc:  # pylint: disable=broad-except
             warn(f">>> Could not download '{project_name}'\n{exc}")
